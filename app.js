f15811c4729b7d8e7c2e252f71dc42b159f9944e
Greg Leung <greg.leung@vantagefx.com>
2019-11-18 15:32:52 +1100

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const product = require('./routes/product.route'); // Imports routes for the products
+const mongoose = require('mongoose');
+
+
+const app = express();
+const PORT = 5555;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+mongoose.connect("mongodb://localhost:27017/ExpressJsCRUD", function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+    }
+  });
+mongoose.Promise = global.Promise;
+
+app.listen(PORT, ()=>{
+    console.log('Service is up and running on port number ' + PORT);
+})
